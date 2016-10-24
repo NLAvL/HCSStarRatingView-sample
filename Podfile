c1217bc2698b73f088daa09f00850a0023963543
Arthur Loo <arthur@vanloo.nu>
2016-10-24 09:52:19 +0200

    initial commit

---
+++ Podfile
@@ -0,0 +1,9 @@
+platform :ios, '9.0'
+
+target 'Rating' do
+  pod 'HCSStarRatingView', '~> 1.4'
+  use_frameworks!
+
+  # Pods for Rating
+
+end
--- Rating/ViewController.swift
+++ Rating/ViewController.swift
@@ -7,12 +7,42 @@
 //
 
 import UIKit
+import HCSStarRatingView
+
+// https://cocoapods.org/?q=HCSStarRatingView
+
+// http://cocoadocs.org/docsets/HCSStarRatingView/1.4.5/
+
+// http://cocoadocs.org/docsets/HCSStarRatingView/1.4.5/Classes/HCSStarRatingView.html
+
+
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var starSet: HCSStarRatingView!
+    
+    @IBOutlet weak var star2Set: HCSStarRatingView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        star2Set.maximumValue = 3
+        star2Set.minimumValue = 2
+        star2Set.value = 2.3
+        star2Set.allowsHalfStars = true
+        star2Set.accurateHalfStars = true
+        star2Set.tintColor = UIColor.yellow
+        
+        
+        if star2Set.value <= star2Set.minimumValue {
+            print("thi is called")
+            star2Set.value = star2Set.minimumValue        }
+        
+        //star2Set.value = 2
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
